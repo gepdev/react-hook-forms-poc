8eea6045726a7d60e8216d9f92f5ca9f53219dc7
Gentian Polovina <gentip@gmail.com>
2022-03-09 05:56:11 +0000

    navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 
 
 import React from "react";
-import { Routes, Route } from "react-router"
+import { Routes, Route, Link } from "react-router-dom"
 import Home from './components/Home'
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={ <Home/> }/>
-    </Routes>
+    <main className="container">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">React Forms</Link>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav me-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to="/">Home</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <Routes>
+        <Route path="/" element={ <Home/> }/>
+      </Routes>
+    </main>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import {
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
   <Router>
